935d7f27772ac2e7a8cafa00e5ef9299980b07dd
MrTreys <omedvedev6666@gmail.com>
2024-01-29 20:15:09 +0800

    Homework finished (I think so)

---
+++ 1task/Program.cs
@@ -0,0 +1,18 @@
+﻿void WriteAllNumbers(int m, int n){
+    if(m > n){
+        Console.WriteLine("m не может быть больше n!");
+    }
+    else
+    Console.Write($"{m} ");
+    m++;
+    if(m < n){
+        WriteAllNumbers(m, n);
+    }
+    else
+    Console.WriteLine();
+}
+Console.WriteLine("Введите m: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите n: ");
+int n = int.Parse(Console.ReadLine()!);
+WriteAllNumbers(m, n);
---
+++ 2task/Program.cs
@@ -0,0 +1,6 @@
+﻿int A(int m, int n){ //Тут я проверил только аргумент m = 2, n = 3.
+    if(m == 0){return n + 1;}
+    if(m > 0 && n == 0){return A(m - 1, 1);}
+    return A(m-1, A(m, n-1));
+}
+Console.WriteLine(A(2, 3));
