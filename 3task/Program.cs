c3bd0f3c6600dd8a7e7f6315e9b206a51fdc247b
MrTreys <omedvedev6666@gmail.com>
2024-01-29 20:20:41 +0800

    Now it's really finished

---
+++ 3task/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] array = {5, 1, 9, 9, 2, 3, 8, -4, 0}; //Можно поменять массив
+void RotateArray(int[] array, int i){
+    Console.Write($"{array[array.Length - i - 1]} ");
+    i++;
+    if(i < array.Length){
+        RotateArray(array, i);
+    }
+}
+RotateArray(array, 0);
